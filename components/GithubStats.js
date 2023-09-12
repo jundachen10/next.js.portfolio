bdfc5c731b36b506df475fbf797d7485fc81f87e
jundachen10 <jundachen10@gmail.com>
2023-09-11 21:47:36 -0400

    feat: github stats card

---
+++ components/GithubStats.js
@@ -0,0 +1,11 @@
+
+const githubReadmeStats = 
+'https://github-readme-stats-gold-alpha.vercel.app/api?username=jundachen10&show_icons=true&theme=radical&hide_rank=true';
+
+export default function GithubStats() {
+    return (
+        <div>
+        <img src={githubReadmeStats} alt="@Junda's github stats" />
+      </div>
+    )
+}
