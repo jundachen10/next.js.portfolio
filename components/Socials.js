7d8446e1946e5dcc86031965b9a3b8b8bd82eaec
jundachen10 <jundachen10@gmail.com>
2023-09-13 18:06:18 -0400

    refactor: socials component pulled out href link and name

--- components/Socials.js
+++ components/Socials.js
@@ -2,14 +2,33 @@ import Link from 'next/link';
 import classes from './Navigation.module.css';
 import styles from '../styles/links.module.scss';
 
+function SocialsBlock({ href, link}) {
+    return(
+        <div className={styles.link}>
+            <a href={{href}}>{link}</a>
+        </div>
+    )
+}
+
 export default function Socials() {
 
     return(
         <>
             <nav>
                 <ul className={classes.list}>
-                    <a href="https://www.linkedin.com/in/jundachen" className={styles.link}>LinkedIn</a>
-                    <a href="https://www.github.com/jundachen10" className={styles.link}>GitHub</a>
+
+                    <SocialsBlock 
+                    href="https://www.linkedin.com/in/jundachen"
+                    link="LinkedIn"
+                    />
+                    <SocialsBlock 
+                    href="https://www.github.com/jundachen10"
+                    link="Git"
+                    />
+
+                    {/*<a href="https://www.linkedin.com/in/jundachen" className={styles.link}>LinkedIn</a>
+                    <a href="https://www.github.com/jundachen10" className={styles.link}>GitHub</a>*/}
+
                 </ul>
             </nav>
         </>
